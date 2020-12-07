2a19be5f5a62cf501b87ef64c116df25ce5a2bc7
aomidi <alirezaomidi@hotmail.com>
2020-12-08 01:11:07 +0330

    Add AdminApp.Infra.Data project and DbContext to Infra.Data Folder

---
+++ AdminApp/AdminApp.Infra.Data/Context/AdminAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AdminApp.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminApp.Infra.Data.Context
+{
+	public class AdminAppDbContext:DbContext
+	{
+		public AdminAppDbContext(DbContextOptions<AdminAppDbContext> options):base(options)
+		{
+			
+		}
+
+		public DbSet<Employee> Employees { get; set; }
+
+	}
+}
