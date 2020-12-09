b2f86fce6338ff15161397c2d8c5d35d7ddc15e5
aomidi <alirezaomidi@hotmail.com>
2020-12-09 19:22:46 +0330

    Implementation Repository and Service

---
+++ AdminApp/AdminApp.Application/Services/EmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using AdminApp.Application.Interfaces;
+using AdminApp.Application.ViewModels;
+using AdminApp.Domain.Interfaces;
+
+namespace AdminApp.Application.Services
+{
+	public class EmployeeService : IEmployeeService
+	{
+		private readonly IEmployeeRepository _employeeRepository;
+
+		public EmployeeService(IEmployeeRepository employeeRepository)
+		{
+			_employeeRepository = employeeRepository;
+		}
+
+		public EmployeeViewModel GetEmployees()
+		{
+			return new EmployeeViewModel()
+			{
+				Employees = _employeeRepository.GetEmployees()
+			};
+		}
+	}
+}
---
+++ AdminApp/AdminApp.Infra.Data/Repository/EmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using AdminApp.Domain.Interfaces;
+using AdminApp.Domain.Models;
+using AdminApp.Infra.Data.Context;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdminApp.Infra.Data.Repository
+{
+	public class EmployeeRepository : IEmployeeRepository
+	{
+		private readonly AdminAppDbContext _context;
+
+		public EmployeeRepository(AdminAppDbContext context)
+		{
+			_context = context;
+		}
+
+		public IEnumerable<Employee> GetEmployees() => _context.Employees;
+		//{
+		//	List<Employee> list = new List<Employee>();
+		//	foreach (var employee in _context.Employees) list.Add(employee);
+		//	return list;
+		//}
+	}
+}
