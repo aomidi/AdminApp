f2dcf6197a962b98cf75ed5b30ac29cbfe9e5957
aomidi <alirezaomidi@hotmail.com>
2020-12-09 19:47:13 +0330

    Implementation IoC

---
+++ AdminApp/AdminApp.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using AdminApp.Application.Interfaces;
+using AdminApp.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdminApp.Infra.IoC
+{
+	public class DependencyContainer
+	{
+		public static void RegisterServices(IServiceCollection service)
+		{
+			//Application Layer
+			service.AddScoped<IEmployeeService, IEmployeeService>();
+
+			//Infra Data Layer
+			service.AddScoped<IEmployeeRepository, IEmployeeRepository>();
+		}
+	}
+}
