fb7029a82af2f042003f10e4e8ad3620d133e7cb
aomidi <alirezaomidi@hotmail.com>
2020-12-11 23:01:04 +0330

    Add Administrator Area

---
+++ AdminApp/AdminApp.Mvc/Areas/Administrator/Views/Shared/Error.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminApp.Mvc.Areas.Administrator.Views.Shared
+{
+    public class ErrorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- AdminApp/AdminApp.Mvc/Startup.cs
+++ AdminApp/AdminApp.Mvc/Startup.cs
@@ -102,6 +102,9 @@ namespace AdminApp.Mvc
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllerRoute(
+					name: "areas",
+					pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+				endpoints.MapControllerRoute(
 					name: "default",
 					pattern: "{controller=Home}/{action=Index}/{id?}");
 				endpoints.MapRazorPages();
