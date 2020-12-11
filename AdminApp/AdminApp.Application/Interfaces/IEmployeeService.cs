ceaeb4e305a9f3660dd997f91850429bebc6954d
aomidi <alirezaomidi@hotmail.com>
2020-12-12 00:14:07 +0330

    Add links in _AdminLayout

--- AdminApp/AdminApp.Application/Interfaces/IEmployeeService.cs
+++ AdminApp/AdminApp.Application/Interfaces/IEmployeeService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AdminApp.Application.ViewModels;
+﻿using AdminApp.Application.ViewModels;
 
 namespace AdminApp.Application.Interfaces
 {
