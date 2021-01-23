6b2933b4893432e0ce758107363b05c7e65f7e52
aomidi <alirezaomidi@hotmail.com>
2021-01-23 14:13:13 +0330

    Create ViewComponent for show Badge

---
+++ AdminApp/AdminApp.Mvc/ViewComponents/ShowBadgeComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AdminApp.Mvc.ViewComponents
+{
+	public class ShowBadgeComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View("/Views/Shared/Components/ShowBadgeComponent.cshtml", "Omidi");
+		}
+
+	}
+}
---
+++ AdminApp/AdminApp.Mvc/ViewComponents/ShowNameComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AdminApp.Mvc.ViewComponents
+{
+	public class ShowNameComponent:ViewComponent
+	{
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View("/Views/Shared/Components/showNameComponent.cshtml", "Alireza");
+		}
+	}
+}
